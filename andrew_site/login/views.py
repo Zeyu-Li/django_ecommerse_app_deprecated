22b80ea0f568c76ccf42e97103112bddb7030b4e
unknown <zeyu.li23@gmail.com>
2019-05-14 19:11:16 -0600

    fixed template

--- andrew_site/login/views.py
+++ andrew_site/login/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def home(request):
 
-    words = {"Home", "Hello World"}
+    words = {"home": "Home", "text":"Hello World"}
     return render(request, 'login/home.html', context={"section": words})
 
 # def login(request):
