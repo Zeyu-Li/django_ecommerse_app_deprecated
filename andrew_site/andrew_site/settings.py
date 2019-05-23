0cdbd0dd5f641bf7bd81f0306efaff432232d87d
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-05-23 11:58:27 -0600

    added requirements.txt

--- andrew_site/andrew_site/settings.py
+++ andrew_site/andrew_site/settings.py
@@ -101,6 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
