75ce875986088d168adf01a800e92dbf57c0c34b
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-05-27 12:04:42 -0600

    Changed and updated template

--- andrew_site/login/views.py
+++ andrew_site/login/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 
+
 def home(request):
 
-    words = {"home": "Home", "text":"Login Project:", "link":"login.html"}
-    return render(request, 'login/home.html', context={"section": words})
-
-def login(request):
-
-    return render(request, 'login/login.html')
+    keywords = {"text": "Login Project:", "page": "home"}
+    return render(request, 'login/home.html', keywords)
