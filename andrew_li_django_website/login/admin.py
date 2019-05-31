df2bcad5a9a04a5163e0cf870c5eaa9166bfba38
unknown <zeyu.li23@gmail.com>
2019-05-30 23:10:30 -0600

    sort users by username

--- andrew_li_django_website/login/admin.py
+++ andrew_li_django_website/login/admin.py
@@ -8,4 +8,9 @@ class UserProfileAdmin(admin.ModelAdmin):
     def user_info(self, obj):
         return obj.email
 
+    def get_queryset(self, request):
+        queryset = super(UserProfileAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('user')
+        return queryset
+
 admin.site.register(UserProfile, UserProfileAdmin)
