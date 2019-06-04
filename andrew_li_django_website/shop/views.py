bf86127b12f3cd2e8eebc8e51a6bc780022edc4f
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-06-04 11:14:40 -0600

    changed base html

--- andrew_li_django_website/shop/views.py
+++ andrew_li_django_website/shop/views.py
@@ -4,16 +4,26 @@ from shop.models import Item
 
 def item_list(request):
     context = {
-        'items': Item.objects.all()
+        # 'items': Item.objects.all(),
+        'page': 'shop'
     }
-    return render(request, 'shop/shop.html')
+
+    return render(request, 'shop/shop.html', context)
 
 
 def product(request):
 
-    return render(request, 'shop/product.html')
+    context = {
+        'page': 'shop'
+    }
+
+    return render(request, 'shop/product.html', context)
 
 
 def checkout(request):
+
+    context = {
+        'page': 'cart'
+    }
 
     return render(request, 'shop/checkout.html')
