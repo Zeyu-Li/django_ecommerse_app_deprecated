6cb6b5262e9c627aa82e4b4f858c519d184fd7f5
unknown <zeyu.li23@gmail.com>
2019-06-03 22:20:54 -0600

    deleted mdb

--- andrew_li_django_website/shop/models.py
+++ andrew_li_django_website/shop/models.py
@@ -2,10 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
+CATEGORY_CHOICES = (
+    ('Tech','Technology'),
+    ('Access', 'Accessories'),
+    ('Trans', 'Transportation'),
+    ('Misc','Other')
+)
+LABEL_CHOICES = (
+    ('N','new'),
+    ('H', 'hot'),
+    ('D', 'danger')
+)
+
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=50)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
 
     def __str__(self):
         return self.title
